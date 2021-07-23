09aff3db56bdd6e026e18073459ed0fc0943b3c2
elena_kau <lelickau@yandex.ru>
2021-07-23 17:27:36 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
-/* new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-		nextEl: '.arrow',
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 20
-		},
-		541: {
-			slidesPerView: 2,
-			spaceBetween: 40
-		}
-	}
-});
-
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.header');
-menuButton.addEventListener('click', function () {
-	menuButton.classList.toggle('menu-button-active');
-	menu.classList.toggle('header-active');
-}) */
-
+/**
+ * Создание элемента html
+ * @param {*String} tagName - имя тэга
+ * @param {*Array ['...']} classNames - массив с классами
+ * @param {*Object {}} attr - объект с атрибутами
+ * @returns - вернет html элемент
+ */
 const getElem = (tagName, classNames, attr) => {
 	const elem = document.createElement(tagName);
 	if (classNames) {
@@ -36,26 +19,28 @@ const getElem = (tagName, classNames, attr) => {
 	return elem;
 }
 
-const createHeader = (param) => {
+/**
+ * Конструктор header
+ * @param {*Jbject} param - объект со значениями для header
+ * @returns - вернет готовый элемент html header
+ */
+const createHeader = ({logo, title,	menu, social}) => {
 	const header = getElem('header');
 	const container = getElem('div', ['container']);
 	const wrapper = getElem('div', ['header']);
-	const btnMenu = getElem('button', ['menu-button'])
 
-	
-
-	if (param.header.logo) {
-		const logo = getElem('img', ['logo'], {
-			src: param.header.logo,
-			alt: param.header.title,
+	if (logo) {
+		const logoElem = getElem('img', ['logo'], {
+			src: logo,
+			alt: title,
 		});
 
-		wrapper.append(logo);
+		wrapper.append(logoElem);
 	}
 
-	if (param.header.menu) {
+	if (menu) {
 		const nav = getElem('nav', ['menu-list']);
-		const allMenuLink = param.header.menu.map(item => {
+		const allMenuLink = menu.map(item => {
 			const link = getElem('a', ['menu-link'], {
 				href: item.link,
 				textContent: item.title
@@ -65,11 +50,19 @@ const createHeader = (param) => {
 
 		nav.append(...allMenuLink);
 		wrapper.append(nav);
+
+		//меню при адаптиве
+		const btnMenu = getElem('button', ['menu-button']);
+		btnMenu.addEventListener('click', () => {
+			btnMenu.classList.toggle('menu-button-active');
+			wrapper.classList.toggle('header-active');
+		});
+		container.append(btnMenu);
 	}
 
-	if (param.header.social) {
+	if (social) {
 		const socialWrapper = getElem('div', ['social']);
-		const allSocial = param.header.social.map(item => {
+		const allSocial = social.map(item => {
 			const socialLink = getElem('a', ['social-link'], {
 				href: item.link
 			});
@@ -87,11 +80,15 @@ const createHeader = (param) => {
 
 	header.append(container);
 	container.append(wrapper);
-	container.append(btnMenu);
 	return header;
 }
 
-const createMain = ({main: {title, genre, rating, descriptions, trailer}}) => {
+/**
+ * Конструктор элемента html main
+ * @param {*Object} param - объект с данными для main 
+ * @returns вернет готовый элемент html main
+ */
+const createMain = ({main: {title, genre, rating, descriptions, trailer, slider}}) => {
 	const main = getElem('main');
 	const container = getElem('div', ['container']);
 	main.append(container);
@@ -104,7 +101,6 @@ const createMain = ({main: {title, genre, rating, descriptions, trailer}}) => {
 		const genreSpan = getElem('span', ['genre','animated','fadeInRight'], {
 			textContent: genre,
 		});
-
 		content.append(genreSpan);
 	}
 
@@ -156,21 +152,99 @@ const createMain = ({main: {title, genre, rating, descriptions, trailer}}) => {
 		youTubeImgLink.append(iconPlay);
 		wrapper.append(youTubeImgLink);
 	}
-	return main;
 
+	if (slider) {
+		const sliderBlock = getElem('div', ['series']);
+		const swiperBlock = getElem('div', ['swiper-container']);
+		const swiperWrapper = getElem('div', ['swiper-wrapper']);
+		const arrow = getElem('button', ['arrow']);
+
+		const slides = slider.map(slide => {
+			const swiperSlide = getElem('div', ['swiper-slide']);
+			const card = getElem('figure', ['card']);
+			swiperSlide.append(card);
+			const cardImg = getElem('img', ['card-img'], {
+				src: slide.img,
+				alt: ((slide.title || 'slide') + ' ' + (slide.subtitle || 'slide')).trim(),
+			});
+			card.append(cardImg);
+
+			if (slide.title || slide.subtitle) {
+				const cardDescription = getElem('figcaption', ['card-description']);
+				cardDescription.innerHTML = `
+					${slide.subtitle ? `<p class="card-subtitle">${slide.subtitle}</p>` : ''}
+					${slide.title ? `<p class="card-title">${slide.title}</p>` : ''}
+					
+				`;
+				card.append(cardDescription);
+			}
+			return swiperSlide
+		});
+		main.append(sliderBlock);
+		sliderBlock.append(swiperBlock, arrow);
+		swiperBlock.append(swiperWrapper);
+		swiperWrapper.append(...slides);
+
+		//swiper slider
+		new Swiper(swiperBlock, {
+			loop: true,
+			navigation: {
+				nextEl: arrow,
+			},
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 20
+				},
+				541: {
+					slidesPerView: 2,
+					spaceBetween: 40
+				}
+			}
+		});
+	}
+	return main;
 }
 
+/**
+ * Финальный конструктор (собирает страницу сайта)
+ * @param {*String} selector - принимает селектор в котором будет находится вся внутренность сайта
+ * @param {*Object} options - объект с данными для контента сайта
+ */
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
 	app.classList.add('body-app');
-
 	app.style.backgroundImage = options.background ? `url('${options.background}')` : '';
-	document.title = options.title;
+	app.style.color = options.fontColor ||  '';
+	app.style.backgroundColor = options.backgroundColor || '';
 
-	if (options.header) {
-		app.append(createHeader(options));
+	if (options.subColor) {
+		document.documentElement.style.setProperty('--sub-color', options.subColor);
 	}
 
+	//title страницы
+	document.title = options.title;
+
+	//favicon
+	if (options.favicon) {
+		const index = options.favicon.lastIndexOf('.');
+		const type = options.favicon.substring(index + 1);
+
+		const favicon = getElem('link', null, {
+			rel:"shortcut icon",
+			href: options.favicon,
+			type: "image/" + (type === 'svg' ? 'svg-xml' : type),
+		});
+		document.head.append(favicon);
+	}
+	
+
+	//header
+	if (options.header) {
+		app.append(createHeader(options.header));
+	}
+
+	//main
 	if (options.main) {
 		app.append(createMain(options));
 	}
@@ -179,6 +253,10 @@ const movieConstructor = (selector, options) => {
 movieConstructor('.app', {
 	title: 'Ведьмак - официальный сайт сериала',
 	background: 'witcher/background.jpg',
+	favicon: 'witcher/logo.png',
+	fontColor: '#FFFFFF',
+	backgroundColor: '#141218',
+	subColor: '#9D2929',
 	header: {
 		title: 'Ведьмак',
 		logo: 'witcher/logo.png',
@@ -218,6 +296,29 @@ movieConstructor('.app', {
 		rating: '8',
 		descriptions: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
 		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+		slider: [
+			{
+				img: 'witcher/series/series-1.jpg',
+				title: 'Начало конца',
+				subtitle: 'Серия №1',
+			},
+			{
+				img: 'witcher/series/series-2.jpg',
+				title: 'Четыре марки',
+				subtitle: 'Серия №2',
+			},
+			{
+				img: 'witcher/series/series-3.jpg',
+				title: 'Предательская луна',
+				subtitle: 'Серия №3',
+			},
+			{
+				img: 'witcher/series/series-4.jpg',
+				title: 'Банкеты, ублюдки и похороны',
+				subtitle: 'Серия №4',
+			}
+		]
 	}
 	
 });
+
